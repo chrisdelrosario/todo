8badad720c17278fa3a20bc8c4a3395959b52a3e
Chris Del Rosario <christian.delrosario@icloud.com>
2018-06-21 11:47:51 -0700

    Initial Commit

---
+++ todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Chris Del Rosario on 6/21/18.
+//  Copyright © 2018 Chris Del Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
